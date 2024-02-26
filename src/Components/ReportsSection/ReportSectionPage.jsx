e9b535c59f4508c094c48d425adcdfc3c3c9e7df
Anahita A. <anahita.amiri103@gmail.com>
2024-02-26 19:54:37 +0300

    starting the report section

--- src/Components/ReportsSection/ReportSectionPage.jsx
+++ src/Components/ReportsSection/ReportSectionPage.jsx
@@ -1,6 +1,6 @@
 function ReportSectionPage() {
   return (
-    <div className="mt-5 mb-5 ml-16 mr-16">
+    <div className="lg:mt-5 lg:mb-5 mt-20 w-full flex flex-col gap-10 lg:grid lg:grid-cols-12 lg:gap-5 lg:justify-between bg-colorD3">
       ReportSectionPage /// Lorem ipsum dolor sit amet consectetur adipisicing
       elit. Magni odio facere molestiae iusto, temporibus impedit! Totam atque
       possimus distinctio rerum doloribus mollitia minus id nobis aperiam, animi

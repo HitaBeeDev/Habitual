8ad5e928c90b704dd14993d21125c860d42626e7
Anahita A. <anahita.amiri103@gmail.com>
2024-02-29 00:13:38 +0300

    finishing states of timetracker

--- src/Components/TimeTracker/TimeTrackerPage.jsx
+++ src/Components/TimeTracker/TimeTrackerPage.jsx
@@ -1,23 +1,45 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import usePersistentState from "../../usePersistentState";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPenToSquare,
+  faSquareCheck,
+  faRectangleXmark,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
 function TimeTrackerPage() {
-  const initialTotalSeconds = 25 * 60; // Initial total seconds for the timer
+  const initialTotalSeconds = 25 * 60;
   const [totalSeconds, setTotalSeconds] = useState(initialTotalSeconds);
   const [projectName, setProjectName] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [timerId, setTimerId] = useState(null);
   const [records, setRecords] = usePersistentState("projectRecords", []);
-  const [isTimerActive, setIsTimerActive] = useState(false); // Track if the timer is active
+  const [isTimerActive, setIsTimerActive] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [editMinutes, setEditMinutes] = useState(
     Math.floor(initialTotalSeconds / 60)
   );
   const [editSeconds, setEditSeconds] = useState(initialTotalSeconds % 60);
-  const shortBreakSeconds = 5 * 60; // 5 minutes break
-  const longBreakSeconds = 15 * 60; // 15 minutes break
-  const [sessionCount, setSessionCount] = useState(0); // Track the number of Pomodoro sessions
-  const [sessionType, setSessionType] = useState("Pomodoro"); // "Pomodoro", "Short Break", "Long Break"
+  const shortBreakSeconds = 5 * 60;
+  const longBreakSeconds = 15 * 60;
+  const [sessionCount, setSessionCount] = useState(0);
+  const [sessionType, setSessionType] = useState("Pomodoro");
+  const radius = 80; // Radius of the SVG circle
+  const circumference = 2 * Math.PI * radius;
+  const [circleStyle, setCircleStyle] = useState({
+    strokeDasharray: circumference,
+    strokeDashoffset: 0,
+  });
+
+  useEffect(() => {
+    const percentage = (totalSeconds / initialTotalSeconds) * 100;
+    const strokeDashoffset = ((100 - percentage) * circumference) / 100;
+    setCircleStyle({
+      strokeDasharray: circumference,
+      strokeDashoffset,
+    });
+  }, [totalSeconds, circumference]);
 
   useEffect(() => {
     if (totalSeconds === 0) {
@@ -44,7 +66,7 @@ function TimeTrackerPage() {
   }, [totalSeconds, sessionType, sessionCount]);
 
   useEffect(() => {
-    return () => clearInterval(timerId); // Cleanup on component unmount
+    return () => clearInterval(timerId);
   }, [timerId]);
 
   const handleProjectNameChange = (e) => {
@@ -52,19 +74,15 @@ function TimeTrackerPage() {
   };
 
   const handleStart = () => {
-    if (projectName.trim() === "") {
-      setErrorMessage("Please enter a project name.");
-      return;
-    }
     setErrorMessage("");
-    setIsTimerActive(true); // Indicate that the timer is now active
+    setIsTimerActive(true);
 
     const id = setInterval(() => {
       setTotalSeconds((prevTotalSeconds) => {
         if (prevTotalSeconds === 0) {
           clearInterval(id);
           setTimerId(null);
-          setIsTimerActive(false); // Timer is no longer active
+          setIsTimerActive(false);
           return 0;
         }
         return prevTotalSeconds - 1;
@@ -78,7 +96,7 @@ function TimeTrackerPage() {
     if (timerId) {
       clearInterval(timerId);
       setTimerId(null);
-      setIsTimerActive(false); // Timer is no longer active
+      setIsTimerActive(false);
     }
   };
 
@@ -86,13 +104,12 @@ function TimeTrackerPage() {
     if (timerId) {
       clearInterval(timerId);
       setTimerId(null);
-      setIsTimerActive(false); // Timer is no longer active
+      setIsTimerActive(false);
     }
     if (totalSeconds !== initialTotalSeconds && projectName.trim() !== "") {
-      // Record the project and time spent before resetting
       recordTimeSpent();
     }
-    // Reset the timer but keep the project name
+
     setTotalSeconds(initialTotalSeconds);
   };
 
@@ -120,8 +137,15 @@ function TimeTrackerPage() {
   };
 
   const handleSaveEdit = () => {
-    const newTotalSeconds = parseInt(editMinutes) * 60 + parseInt(editSeconds);
+    // Ensure the inputs are not negative and within reasonable bounds
+    const newMinutes = Math.max(0, Math.min(999, parseInt(editMinutes)));
+    const newSeconds = Math.max(0, Math.min(59, parseInt(editSeconds)));
+    const newTotalSeconds = newMinutes * 60 + newSeconds;
+
+    // Update the timer with the new total seconds
     setTotalSeconds(newTotalSeconds);
+
+    // Exit editing mode
     setIsEditing(false);
   };
 
@@ -131,119 +155,184 @@ function TimeTrackerPage() {
   };
 
   const sumAllTimes = () => {
-    // Sum all the times from the records
     const totalSecondsFromRecords = records.reduce((acc, record) => {
       return acc + timeToSeconds(record.timePassed);
     }, 0);
 
-    // Calculate the time passed for the currently active timer, if any
-    const currentSessionTimePassed = isTimerActive
-      ? initialTotalSeconds - totalSeconds
-      : 0;
+    // Add total seconds from records to initial total seconds
+    let totalSeconds = initialTotalSeconds + totalSecondsFromRecords;
 
-    // Sum the total seconds
-    const totalSeconds = totalSecondsFromRecords + currentSessionTimePassed;
-
-    // Convert the total seconds to hours, minutes, and seconds
+    // Convert total seconds to hours, minutes, and seconds
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
 
-    // Format the output
     return `${hours} hour, ${minutes} minutes, ${seconds} seconds`;
   };
 
+  const deleteProject = (indexToDelete) => {
+    const updatedRecords = records.filter(
+      (_, index) => index !== indexToDelete
+    );
+
+    setRecords(updatedRecords);
+  };
+
   return (
-    <div className="flex flex-col gap-10 bg-colorA2">
-      <div className="flex flex-col gap-10 bg-colorA3">
-        <div className="flex flex-row gap-10">
-          <p style={{ color: sessionType === "Pomodoro" ? "green" : "black" }}>
+    <div className="lg:mt-5 lg:mb-5 mt-20 w-full flex flex-col gap-5 lg:grid lg:grid-cols-12">
+      <div className="col-span-6 bg-colorJ1 shadow-xl rounded-lg flex flex-col gap-5 justify-between p-10 items-center">
+        <div className="flex flex-row gap-5 justify-center items-center shadow-xl rounded-lg">
+          <p
+            className={`text-md font-semibold w-32 text-center flex flex-col items-center justify-center h-10 rounded-lg text-colorJ10 ${
+              sessionType === "Pomodoro" ? "bg-colorJ22" : "bg-colorJ2"
+            }`}
+          >
             Pomodoro
           </p>
+
           <p
-            style={{ color: sessionType === "Short Break" ? "green" : "black" }}
+            className={`text-md font-semibold w-32 text-center flex flex-col items-center justify-center h-10 rounded-lg text-colorJ10 ${
+              sessionType === "Short Break" ? "bg-colorJ23" : "bg-colorJ2"
+            }`}
           >
             Short Break
           </p>
+
           <p
-            style={{ color: sessionType === "Long Break" ? "green" : "black" }}
+            className={`text-md font-semibold w-32 text-center flex flex-col items-center justify-center h-10 rounded-lg text-colorJ10 ${
+              sessionType === "Long Break" ? "bg-colorJ24" : "bg-colorJ2"
+            }`}
           >
             Long Break
           </p>
         </div>
 
-        <div className="flex flex-row gap-5">
-          <label>Project name?</label>
-          <input
-            type="text"
-            placeholder="Type your project name"
-            value={projectName}
-            onChange={handleProjectNameChange}
-          />
-          <button className="bg-colorC3" onClick={handleStart}>
+        <div className="relative flex flex-row justify-center items-center">
+          <svg
+            width="350"
+            height="350"
+            viewBox="0 0 200 200"
+            className="absolute"
+          >
+            <circle
+              className="timer-circle-bg"
+              stroke="#e6e6e6"
+              cx="100"
+              cy="100"
+              r={radius}
+              strokeWidth="4"
+              fill="transparent"
+            />
+            <circle
+              className="timer-circle-progress"
+              stroke="#CBF1F5"
+              cx="100"
+              cy="100"
+              r={radius}
+              strokeWidth="4"
+              fill="transparent"
+              style={{
+                strokeDasharray: `${circumference} ${circumference}`,
+                strokeDashoffset: `var(--stroke-dashoffset)`,
+                transition: "stroke-dashoffset 1s linear",
+                transform: "rotate(-90deg)",
+                transformOrigin: "50% 50%",
+              }}
+            />
+          </svg>
+
+          <div className="flex flex-row justify-center items-center">
+            {!isEditing ? (
+              <p className="text-colorJ11 font-semibold tracking-widest text-5xl">
+                {`${Math.floor(totalSeconds / 60) < 10 ? "0" : ""}${Math.floor(
+                  totalSeconds / 60
+                )}:${totalSeconds % 60 < 10 ? "0" : ""}${totalSeconds % 60}`}
+              </p>
+            ) : (
+              <div className="flex flex-row justify-center gap-2 items-center">
+                <input
+                  className="text-center w-12 rounded-md p-1"
+                  type="number"
+                  value={editMinutes}
+                  onChange={(e) => setEditMinutes(e.target.value)}
+                />
+                <span>:</span>
+                <input
+                  className="text-center w-12 rounded-md p-1 mr-2"
+                  type="number"
+                  value={editSeconds}
+                  onChange={(e) => setEditSeconds(e.target.value)}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-5 justify-center items-center">
+          <div className="flex flex-row justify-center items-center gap-2">
+            {isEditing ? (
+              <button onClick={handleSaveEdit}>
+                <button
+                  className="text-md font-semibold w-32 text-center flex 
+              flex-col items-center justify-center h-10 rounded-lg
+               text-colorJ10 bg-colorJ28"
+                >
+                  SAVE
+                </button>
+              </button>
+            ) : null}
+
+            <div onClick={() => setIsEditing(!isEditing)}>
+              {isEditing ? (
+                <button
+                  className="text-md font-semibold w-32 text-center flex 
+              flex-col items-center justify-center h-10 rounded-lg
+               text-colorJ10 bg-colorJ29"
+                >
+                  CANCEL
+                </button>
+              ) : (
+                <button
+                  className="text-md font-semibold w-32 text-center flex 
+                flex-col items-center justify-center h-10 rounded-lg
+                 text-colorJ10 bg-colorJ30"
+                >
+                  custom time??
+                </button>
+              )}
+            </div>
+          </div>
+
+          <button
+            className="text-md font-semibold w-32 text-center flex 
+            flex-col items-center justify-center h-10 rounded-lg
+             text-colorJ10 bg-colorJ12"
+            onClick={handleStart}
+          >
             Start
           </button>
-          <button className="bg-colorB5" onClick={handlePause}>
+
+          <button
+            className="text-md font-semibold w-32 text-center flex 
+            flex-col items-center justify-center h-10 rounded-lg
+             text-colorJ10 bg-colorJ13"
+            onClick={handlePause}
+          >
             Pause
           </button>
-          <button onClick={handleReset} className="bg-colorC1">
+
+          <button
+            onClick={handleReset}
+            className="text-md font-semibold w-32 text-center flex 
+            flex-col items-center justify-center h-10 rounded-lg
+             text-colorJ10 bg-colorJ14"
+          >
             Reset
           </button>
         </div>
-
-        <div>
-          {!isEditing ? (
-            <p className="bg-colorB1">
-              {`${Math.floor(totalSeconds / 60) < 10 ? "0" : ""}${Math.floor(
-                totalSeconds / 60
-              )}:${totalSeconds % 60 < 10 ? "0" : ""}${totalSeconds % 60}`}
-            </p>
-          ) : (
-            <div>
-              <input
-                type="number"
-                value={editMinutes}
-                onChange={(e) => setEditMinutes(e.target.value)}
-              />
-              <span>:</span>
-              <input
-                type="number"
-                value={editSeconds}
-                onChange={(e) => setEditSeconds(e.target.value)}
-              />
-              <button onClick={handleSaveEdit}>Save</button>
-            </div>
-          )}
-          <button onClick={() => setIsEditing(!isEditing)}>
-            {isEditing ? "Cancel" : "Edit"}
-          </button>
-        </div>
-
-        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       </div>
 
-      <div className="bg-colorC2">
-        {records.length > 0 || isTimerActive ? (
-          <ul>
-            {records.map((record, index) => (
-              <li key={index}>
-                <p>Project: {record.projectName}</p>
-                <p>Time Passed: {record.timePassed}</p>
-              </li>
-            ))}
-            {isTimerActive && (
-              <li>
-                <p>Project: {projectName}</p>
-                <p>Time Passed: {calculateTimePassed()}</p>
-              </li>
-            )}
-          </ul>
-        ) : (
-          <p>No records available</p>
-        )}
-      </div>
-
-      <p>you studied total of: {sumAllTimes()}</p>
+      <div className="col-span-6 gap-5 overflow-auto bg-colorJ3 shadow-xl rounded-lg"></div>
     </div>
   );
 }

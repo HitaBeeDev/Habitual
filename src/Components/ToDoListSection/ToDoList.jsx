f51ea1d23a844b4282c86131c99ceba30ddff77a
Anahita A. <anahita.amiri103@gmail.com>
2024-02-28 08:46:26 +0300

    todo list

--- src/Components/ToDoListSection/ToDoList.jsx
+++ src/Components/ToDoListSection/ToDoList.jsx
@@ -17,7 +17,7 @@ function ToDoList() {
   } = useTasks();
 
   return (
-    <div className="hidden lg:block mt-5 bg-colorB2 rounded-lg shadow-xl p-1 pl-5 pr-5 pb-5">
+    <div className="hidden lg:block mt-5 bg-colorB3/20 rounded-lg shadow-xl p-1 pl-5 pr-5 pb-5">
       {sortedTasks.map(([date, tasks]) => (
         <div key={date}>
           <p className=" text-sm font-semibold mb-2 mt-5">{date}</p>
@@ -33,16 +33,20 @@ function ToDoList() {
                       : "none",
                   }}
                 >
-                  <div className="hidden lg:grid grid-cols-12 pl-3 pr-3 gap-3 items-center rounded-lg shadow-xl bg-colorB1 h-14">
-                    <div className="col-span-3 rounded-lg border-2 border-colorB2 h-10 flex flex-row gap-3 justify-start pl-5 items-center">
+                  <div className="hidden lg:grid  bg-colorB1 h-20 grid-cols-12 pl-3 pr-3 gap-5 items-center rounded-lg shadow-xl">
+                    <div className="col-span-4 flex flex-row gap-5 justify-start items-center">
                       <input
-                        className="w-4 h-4"
+                        className={`cursor-pointer w-5 border border-colorB5 h-5 rounded-full ${
+                          checkedTasks.includes(taskIdentifier)
+                            ? "bg-colorB5"
+                            : "bg-colorB5/10"
+                        } appearance-none`}
                         type="checkbox"
                         checked={checkedTasks.includes(taskIdentifier)}
                         onChange={() => handleCheckboxChange(taskIdentifier)}
                       />
 
-                      <div>
+                      <div className="flex flex-col gap-2">
                         {isEditing && editTaskIndex === index ? (
                           <input
                             type="text"
@@ -52,64 +56,62 @@ function ToDoList() {
                             }
                           />
                         ) : (
-                          <p className="text-sm font-semibold text-colorB4">
+                          <p className="tracking-widest	text-sm font-semibold text-colorB4">
                             {task.name}
                           </p>
                         )}
+
+                        <p className="text-center text-xs font-normal text-colorB4">
+                          {isEditing && editTaskIndex === index ? (
+                            <input
+                              type="text"
+                              value={newTask.description}
+                              onChange={(e) =>
+                                updateNewTask("description", e.target.value)
+                              }
+                            />
+                          ) : (
+                            task.description
+                          )}
+                        </p>
                       </div>
                     </div>
 
-                    <div className="col-span-3 h-10 flex items-center justify-center rounded-lg border-2 border-colorB2">
-                      <p className="text-center text-sm font-normal text-colorB4">
-                        {isEditing && editTaskIndex === index ? (
-                          <input
-                            type="text"
-                            value={newTask.description}
-                            onChange={(e) =>
-                              updateNewTask("description", e.target.value)
-                            }
-                          />
-                        ) : (
-                          task.description
-                        )}
-                      </p>
-                    </div>
-
-                    <div className="col-span-2 h-10 flex items-center justify-center rounded-lg border-2 border-colorB2">
-                      <p className="text-center text-sm font-medium text-colorB4">
+                    <div className="col-span-2 flex items-center justify-center">
+                      <p className="text-center tracking-widest text-sm font-medium text-colorB4">
                         {task.date}
                       </p>
                     </div>
 
-                    <div className="col-span-1 h-10 flex items-center justify-center rounded-lg border-2 border-colorB2">
-                      <p className="text-center text-sm font-medium text-colorB4">
+                    <div className="col-span-1 flex items-center justify-center">
+                      <p className="text-center text-sm tracking-widest	 font-medium text-colorB4">
                         {task.startTime}
                       </p>
                     </div>
 
-                    <div className="col-span-1 h-10 flex items-center justify-center rounded-lg border-2 border-colorB2">
-                      <p className="text-center text-sm font-medium text-colorB4">
+                    <div className="col-span-1 flex items-center justify-center">
+                      <p className="text-center text-sm tracking-widest	 font-medium text-colorB4">
                         {task.endTime}
                       </p>
                     </div>
 
                     <div
-                      className={`col-span-1 h-10 flex items-center justify-center rounded-lg border-2 border-colorB2 ${
+                      className={`col-span-2 flex items-center justify-center rounded-lg h-10 ${
                         task.priority === "High"
-                          ? "bg-colorC2 border-colorC2"
+                          ? "bg-colorC2 border-colorC2 text-colorA2"
                           : task.priority === "Medium"
-                          ? "bg-colorA5 border-colorA5"
+                          ? "bg-colorA5 border-colorA5 text-colorB4"
                           : task.priority === "Low"
-                          ? "bg-colorC3 border-colorC3"
+                          ? "bg-colorC3 border-colorC3 text-colorB4"
                           : ""
                       }`}
                     >
-                      <p className="text-center text-sm font-medium text-colorB4">
+                      <p className="text-center text-sm font-medium">
                         {task.priority}
                       </p>
                     </div>
 
-                    <div className="col-span-1 flex justify-center gap-3 items-center">
+                    <div className="col-span-2 flex justify-center gap-4 items-center">
                       <button onClick={() => handleTaskEditClick(task.id)}>
                         <FontAwesomeIcon
                           icon={faPenToSquare}
--- src/Components/ToDoListSection/UpcomingTasks.jsx
+++ src/Components/ToDoListSection/UpcomingTasks.jsx
@@ -4,10 +4,10 @@ function UpcomingTasks() {
   const { groupedTasks, checkedTasks, generateTaskIdentifier } = useTasks();
 
   return (
-    <div className="col-span-3 bg-colorB2 shadow-xl rounded-lg p-3">
+    <div className="col-span-4 bg-colorB3/20 shadow-xl rounded-lg p-3">
       <p className="text-sm font-semibold mb-3">Upcoming Tasks</p>
 
-      <div className="bg-colorB1 shadow-xl rounded-lg p-3">
+      <div>
         {Object.entries(groupedTasks).map(([date, tasks], index) => (
           <div key={date}>
             {index !== 0 && <div style={{ marginBottom: "20px" }}></div>}
@@ -22,7 +22,7 @@ function UpcomingTasks() {
                   <li key={index}>
                     <p className="text-xs font-semibold text-colorB4">{date}</p>
 
-                    <div className="grid grid-cols-12 gap-1 mt-2 items-center justify-between">
+                    <div className="shadow-xl rounded-lg p-3 bg-colorB1 grid grid-cols-12 gap-1 mt-2 items-center justify-between">
                       <p className="col-span-4 text-center text-xs font-semibold text-colorB4 h-7 p-1 rounded-lg border-2 border-colorB2">
                         {task.name}
                       </p>

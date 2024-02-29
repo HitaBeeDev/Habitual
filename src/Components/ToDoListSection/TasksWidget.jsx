38a0c803d4e0c6b0d29c07aecd3f8148fc3d030d
Anahita A. <anahita.amiri103@gmail.com>
2024-02-29 22:25:31 +0300

    changed some classes

--- src/Components/ToDoListSection/TasksWidget.jsx
+++ src/Components/ToDoListSection/TasksWidget.jsx
@@ -8,66 +8,73 @@ function TasksWidget() {
     return new Date(a[0]) - new Date(b[0]);
   };
 
+  const upcomingTasks = Object.entries(groupedTasks)
+    .sort(compareDates)
+    .slice(0, 2)
+    .map(([date, tasks]) =>
+      tasks
+        .filter(
+          (task, index) =>
+            !checkedTasks.includes(generateTaskIdentifier(task, index))
+        )
+        .slice(0, 2)
+    )
+    .flat();
+
   return (
     <div className="cursor-pointer p-5 bg-colorH4 rounded-md shadow-xl h-full">
       <p className="text-lg font-semibold text-colorB4">📝 Upcoming Plans: </p>
 
       <div className="mt-3">
-        {Object.entries(groupedTasks)
-          .sort(compareDates)
-          .slice(0, 2) // Limit to first 2 groups
-          .map(([date, tasks], index) => (
-            <div key={date}>
+        {upcomingTasks.length > 0 ? (
+          upcomingTasks.map((task, index) => (
+            <div key={index}>
               {index !== 0 && <div style={{ marginBottom: "20px" }}></div>}
 
               <ul>
-                {tasks
-                  .filter(
-                    (task, index) =>
-                      !checkedTasks.includes(
-                        generateTaskIdentifier(task, index)
-                      )
-                  )
-                  .slice(0, 2)
-                  .map((task, index) => (
-                    <li key={index}>
-                      <p className="text-xs text-colorB4 font-semibold">
-                        {date}
+                <li>
+                  <p className="text-xs text-colorB4 font-semibold">
+                    {task.date}
+                  </p>
+
+                  <div
+                    className={`mt-2 flex p-2 flex-row rounded-lg justify-center gap-5 ${
+                      task.priority === "High"
+                        ? "border-l-8 border-colorC2"
+                        : task.priority === "Medium"
+                        ? " border-colorA5 border-l-8"
+                        : task.priority === "Low"
+                        ? " border-colorC3 border-l-8"
+                        : ""
+                    }`}
+                  >
+                    <div className="flex flex-col gap-1">
+                      <p className="text-sm font-semibold text-colorI1">
+                        {task.name}
                       </p>
+                      <p className="text-xs font-normal text-colorI1">
+                        {task.description}
+                      </p>
+                    </div>
 
-                      <div
-                        className={`mt-2 flex p-2 flex-row rounded-lg justify-center gap-5 ${
-                          task.priority === "High"
-                            ? "border-l-8 border-colorC2"
-                            : task.priority === "Medium"
-                            ? " border-colorA5 border-l-8"
-                            : task.priority === "Low"
-                            ? " border-colorC3 border-l-8"
-                            : ""
-                        }`}
-                      >
-                        <div className="flex flex-col gap-1">
-                          <p className="text-sm font-semibold text-colorI1">
-                            {task.name}
-                          </p>
-                          <p className="text-xs font-normal text-colorI1">
-                            {task.description}
-                          </p>
-                        </div>
+                    <p className="text-xs font-medium text-colorI1">
+                      {task.startTime} - {task.endTime}
+                    </p>
 
-                        <p className="text-xs font-medium text-colorI1">
-                          {task.startTime} - {task.endTime}
-                        </p>
-
-                        <p className="text-sm font-semibold text-colorI1">
-                          {task.priority}
-                        </p>
-                      </div>
-                    </li>
-                  ))}
+                    <p className="text-sm font-semibold text-colorI1">
+                      {task.priority}
+                    </p>
+                  </div>
+                </li>
               </ul>
             </div>
-          ))}
+          ))
+        ) : (
+          <p className="text-sm flex font-semibold text-colorB4">
+            Everything's all set, no tasks ahead! Take a moment to breathe and
+            enjoy your day.
+          </p>
+        )}
       </div>
     </div>
   );
